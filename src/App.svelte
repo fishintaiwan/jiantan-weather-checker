52515b3ad4efa38e28f593b58755aa381af1d758
fishintaiwan <fershad@gmail.com>
2020-04-22 13:23:00 +0800

    config footer, create service worker

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,25 @@
 </script>
 
 <style>
+  .text-link {
+    font-weight: bold;
+    text-decoration: underline;
+    transition: all 0.3s;
+    color: var(--teal-green);
+  }
 
+  .text-link:hover {
+    color: var(--pale-grey);
+    text-decoration: none;
+  }
+
+  .footer {
+    min-height: 25vh;
+    background: var(--bg-secondary);
+    margin-top: 2em;
+    position: relative;
+    color: var(--primary-text);
+  }
 </style>
 
 <main>
@@ -14,3 +32,18 @@
     <CCTV />
   </div>
 </main>
+
+<footer class="footer">
+  <div class="container">
+    <p>
+      Made by
+      <a href="https://www.fershad.com" class="text-link">Fershad Irani</a>
+    </p>
+    <p>
+      If you find this handy, please consider supporting my work.
+      <a href="https://www.buymeacoffee.com/fishintaiwan" class="text-link">
+        Buy me a coffee
+      </a>
+    </p>
+  </div>
+</footer>
